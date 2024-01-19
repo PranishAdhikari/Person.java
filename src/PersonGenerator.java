dac6a2c180545afbc818a21de0dc6e3addbcf5f2
pranish <adhikapi@mail.uc.edu>
2024-01-19 02:14:19 -0500

    Initial commit

---
+++ src/PersonGenerator.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PersonGenerator {
+    public static void main(String[] args)
+    {
+        ArrayList<Person> personOData = new ArrayList<>();
+
+        Scanner in = new Scanner(System.in);
+        String ID = "";
+        String firstName = "";
+        String lastName = "";
+        String title = "";
+        int YOB = 0;
+        char DQ = '\u0022';
+
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonTestData.txt");
+
+        boolean done = false;
+        do {
+            ID = SafeInput.getNonZeroLenString(in, "Enter the ID as 6 digits");
+            firstName = SafeInput.getNonZeroLenString(in, "Enter the First Name");
+            lastName = SafeInput.getNonZeroLenString(in, "Enter the Last Name");
+            title = SafeInput.getNonZeroLenString(in, "Enter the Last Title");
+            YOB = SafeInput.getRangedInt(in,"Enter the year of birth as 4 digits", 1000, 9999);
+
+
+            personOData.add(new Person(ID,firstName, lastName, title, YOB));
+
+
+
+            done = SafeInput.getYNConfirm(in, "Are you done entering Person data?");
+
+
+        } while (!done);
+
+        for (Person p: personOData) {
+            System.out.println(p.toCSVRecord());
+        }
+
+        System.out.println();
+        System.out.println("<Person>");
+        for (Person p: personOData) {
+            System.out.println(p.toXMLRecord());
+        }
+        System.out.println("</Person>");
+        System.out.println();
+
+        System.out.println("{" + DQ + "Person" + DQ + ":[");
+        for (Person p: personOData) {
+            System.out.println(p.toJSONRecord());
+        }
+        System.out.println("]}");
+        try
+        {
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            for(Person rec : personOData)
+            {
+                writer.write(rec.toCSVRecord());
+                writer.newLine();
+            }
+            writer.close();
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/PersonReader.java
@@ -0,0 +1,94 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PersonReader {
+    public static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec;
+        ArrayList<String> lines = new ArrayList<>();
+        ArrayList<Person> personOData = new ArrayList<>();
+        char DQ = '\u0022';
+
+        final int FIELDS_LENGTH = 5;
+
+        String ID = "", firstName = "", lastName = "", title = "";
+        int YOB = 0;
+
+        try {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                int line = 0;
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    lines.add(rec);
+                    line++;
+                }
+                reader.close();
+
+                String[] fields;
+                for (String l : lines) {
+                    fields = l.split(",");
+                    if (fields.length == FIELDS_LENGTH) {
+                        ID = fields[0].trim();
+                        firstName = fields[1].trim();
+                        lastName = fields[2].trim();
+                        title = fields[3].trim();
+                        try {
+                            YOB = Integer.parseInt(fields[4].trim());
+                            personOData.add(new Person(ID, firstName, lastName, title, YOB));
+                        } catch (NumberFormatException e) {
+                            System.out.println("Error parsing 'YOB' field as integer.");
+                            System.out.println("Invalid value: " + fields[4].trim());
+                            // Handle or skip the invalid record as needed
+                        }
+                    } else {
+                        System.out.println("Found a record that may be corrupt: ");
+                        System.out.println();
+                    }
+                }
+            } else {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
+            }
+
+            for (Person p : personOData) {
+                System.out.println(p.toCSVRecord());
+            }
+
+            System.out.println();
+            System.out.println("<Person>");
+            for (Person p : personOData) {
+                System.out.println(p.toXMLRecord());
+            }
+            System.out.println("</Person>");
+            System.out.println();
+
+            System.out.println("{" + DQ + "Person" + DQ + ":[");
+            for (Person p : personOData) {
+                System.out.println(p.toJSONRecord());
+            }
+            System.out.println("]}");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
